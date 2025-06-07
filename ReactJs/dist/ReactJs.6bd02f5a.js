2d531c14ae9ad263ec864b4ca9e13427132b4fe0
jnSakshi22 <jnsakshi.1998@gmail.com>
2025-06-07 14:00:32 +0530

    Javascript and React concepts

---
+++ ReactJs/dist/ReactJs.6bd02f5a.js
@@ -0,0 +1,35 @@
+const parent = React.createElement("div", {
+    id: "parent"
+}, [
+    React.createElement("div", {
+        id: "child1"
+    }, [
+        React.createElement("h1", {}, "I am an h1 tag."),
+        React.createElement("h2", {}, "I am an h2 tag.")
+    ]),
+    React.createElement("div", {
+        id: "child2"
+    }, [
+        React.createElement("h1", {}, "I am an h1 tag."),
+        React.createElement("h2", {}, "I am an h2 tag.")
+    ])
+]);
+const heading = React.createElement("h1", {
+    id: "heading",
+    xyz: "abc"
+}, "Hello World From React");
+console.log("Heading by React: ", heading); // return object
+// React.createElement() is a function provided by the React library that creates and returns a React element. A React element is a plain JavaScript object that describes what you want to display on the screen.
+// Syntax - React.createElement(type, props, ...children)
+// Parameters
+// type: The type of the element you want to create. It can be:
+// A string (e.g., 'div', 'span') for HTML elements.
+// A React component (e.g., MyComponent) for custom components.
+// props: An object containing the attributes or properties for the element (e.g., className, id, etc.). Pass null if there are no props.
+// ...children: The child elements or content inside the element. These can be strings, numbers, other React elements, or arrays of elements.
+const root = ReactDOM.createRoot(document.getElementById("root"));
+// console.log("Root by React: ", root); // return object
+// root.render(heading);
+root.render(parent);
+
+//# sourceMappingURL=ReactJs.6bd02f5a.js.map
